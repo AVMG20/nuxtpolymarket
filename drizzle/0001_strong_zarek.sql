ac78328da4bb6f742f3bb4062a93afebe0a38638
AVMG20 <rijstenpap1234@hotmail.com>
2026-08-04 20:43:19 +0200

    add database migration for prestige

---
+++ drizzle/0001_strong_zarek.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "prestige_purchases" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"item_id" text NOT NULL,
+	"count" integer DEFAULT 0 NOT NULL,
+	CONSTRAINT "prestige_purchases_unique" UNIQUE("user_id","item_id")
+);
+--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "prestige" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "prestige_tokens" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "prestige_purchases" ADD CONSTRAINT "prestige_purchases_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "prestige_purchases_userId_idx" ON "prestige_purchases" USING btree ("user_id");
